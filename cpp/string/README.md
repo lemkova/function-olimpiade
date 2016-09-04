98fdafae94475b5bc9f4380a73ad96fd73797970
Okki <graceda64@gmail.com>
2016-09-04 21:02:35 +0700

    Add cpp folder

---
+++ cpp/string/README.md
@@ -0,0 +1,3 @@
+## C++ - String
+1. IsVowel
+	Untuk mengecek apakah string tersebut termasuk huruf vokal atau bukan.
---
+++ cpp/string/isVowel.cpp
@@ -0,0 +1,8 @@
+#include <cstring>
+
+bool isVocal(string a){
+    if(a == "a" || a == "i" || a == "u" || a == "e" || a == "o" || a == "A" || a == "I" || a == "U" || a == "E" || a == "O"){
+        return true;
+    }
+    return false;
+}
