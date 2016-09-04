df256eb43646350b8ca7cc7991f6edc2dfd3150a
Muhammad Okki <graceda64@gmail.com>
2016-09-04 22:58:29 +0700

    Update isVowel.cpp
    cek 1 huruf saja

--- cpp/string/isVowel.cpp
+++ cpp/string/isVowel.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 
 bool isVocal(string a){
+    if(a.length() != 1) return false; // Hanya cek string yang berisi 1 huruf
     if(a == "a" || a == "i" || a == "u" || a == "e" || a == "o" || a == "A" || a == "I" || a == "U" || a == "E" || a == "O"){
         return true;
     }
