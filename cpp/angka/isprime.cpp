d427fcbec123e4aa6d1883b15f7fb870cfe174a5
Muhammad Okki <graceda64@gmail.com>
2016-09-06 11:53:54 +0700

    Add isprime.cpp

---
+++ cpp/angka/isprime.cpp
@@ -0,0 +1,24 @@
+#include <math.h>
+bool isPrime (int num)
+{
+    if (num <=1)
+        return false;
+    else if (num == 2)         
+        return true;
+    else if (num % 2 == 0)
+        return false;
+    else
+    {
+        bool prime = true;
+        int divisor = 3;
+        double num_d = static_cast<double>(num);
+        int upperLimit = static_cast<int>(sqrt(num_d) +1);
+        
+        while (divisor <= upperLimit)
+        {
+            if (num % divisor == 0)
+                prime = false;
+            divisor +=2;
+        }
+        return prime;
+    }
